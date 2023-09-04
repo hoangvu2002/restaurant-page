ecf121303dd22a7f93487bd9cd05cfc6118ecbce
pdhoangvu <pdhoangvu@gmail.com>
2023-09-04 19:48:05 +0700

    Create a page-load module and style.css

---
+++ src/page-load.js
@@ -0,0 +1,6 @@
+export default function pageLoad() {
+    const content = document.getElementById('content');
+    const logo = document.createElement('img');
+    const heading = document.createElement('h1');
+    const desc = document.createElement('p');
+}
