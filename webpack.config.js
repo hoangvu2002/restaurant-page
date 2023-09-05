c27d69b6947f03d60bf040872d9057770f159427
pdhoangvu <pdhoangvu@gmail.com>
2023-09-05 13:15:20 +0700

    Change the config file for font loading

--- webpack.config.js
+++ webpack.config.js
@@ -12,8 +12,12 @@ module.exports = {
         test: /\.css$/i,
         use: ['style-loader', 'css-loader'],
       },
+      {
+        test: /\.(png|svg|jpg|jpeg|gif)$/i,
+        type: 'asset/resource',
+      },
      {
-       test: /\.(png|svg|jpg|jpeg|gif)$/i,
+       test: /\.(woff|woff2|eot|ttf|otf)$/i,
        type: 'asset/resource',
      },
     ],
