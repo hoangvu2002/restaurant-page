d8bc65b7b27882637d2504f12bb2fef9dbb4803e
pdhoangvu <pdhoangvu@gmail.com>
2023-09-05 10:48:23 +0700

    Set webpack config for loading images

--- src/page-load.js
+++ src/page-load.js
@@ -1,6 +1,8 @@
+import myImage from './restaurant.png';
 export default function pageLoad() {
     const content = document.getElementById('content');
     const logo = document.createElement('img');
     const heading = document.createElement('h1');
     const desc = document.createElement('p');
+    logo.src = myImage;
 }
