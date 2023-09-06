3c545b1e25ed2e67f310459c06e3e8fe5fe146a7
pdhoangvu <pdhoangvu@gmail.com>
2023-09-06 14:27:45 +0700

    Add styling to the menu tab

--- src/menu.js
+++ src/menu.js
@@ -16,13 +16,15 @@ export default function menuTab() {
     menuItem1Desc.textContent = 'Start your meal with our tantalizing appetizers, including bruschetta, stuffed mushrooms, and crispy calamari. Perfect for sharing or savoring on your own.';
     menuItem1.appendChild(menuItem1Heading);
     menuItem1.appendChild(menuItem1Desc);
+    menuItem1.classList.add('menu-item');
     const menuItem2 = document.createElement('div');
     const menuItem2Heading = document.createElement('h3');
     const menuItem2Desc = document.createElement('p');
     menuItem2Heading.textContent = 'Entrees';
     menuItem2Desc.textContent = 'Indulge in our savory entrees, featuring a variety of dishes such as coq au vin, seafood paella, and steak au poivre. Each dish is prepared to perfection.';
-    menuItem2.appendChild(menuItem1Heading);
-    menuItem2.appendChild(menuItem1Desc);
+    menuItem2.appendChild(menuItem2Heading);
+    menuItem2.appendChild(menuItem2Desc);
+    menuItem2.classList.add('menu-item');
     const menuItem3 = document.createElement('div');
     const menuItem3Heading = document.createElement('h3');
     const menuItem3Desc = document.createElement('p');
@@ -30,6 +32,7 @@ export default function menuTab() {
     menuItem3Desc.textContent = 'Complete your dining experience with our heavenly desserts. From rich chocolate fondant to classic crème brûlée, our desserts will satisfy your sweet cravings.';
     menuItem3.appendChild(menuItem3Heading);
     menuItem3.appendChild(menuItem3Desc);
+    menuItem3.classList.add('menu-item');
     container.appendChild(menuItem1);
     container.appendChild(menuItem2);
     container.appendChild(menuItem3);
