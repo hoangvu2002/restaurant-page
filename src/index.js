45f4c1b47608901657f4f6de1233d76736e6119b
pdhoangvu <pdhoangvu@gmail.com>
2023-09-06 15:08:26 +0700

    Create contact generating module

--- src/index.js
+++ src/index.js
@@ -2,5 +2,6 @@ import './style.css';
 import pageLoad from './page-load.js';
 import homeTab from './home';
 import menuTab from './menu';
+import contactTab from './contact';
 pageLoad();
-menuTab();
+contactTab();
